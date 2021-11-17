3aae823af77856c6a5cb7aedf20addfa31d4d937
AmanHashTrust <aman@hashtrust.in>
2021-11-17 11:40:56 +0530

    PR #1 - Added(Project) - Added app with initial task and commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import UserInput from "./UserInput";
 
 function App() {
+  const [value, setValue] = useState("");
+  const [text, setText] = useState("Change Me!!");
+
+  //? Setting input value on every Key press Event
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  };
+
+  const onButtonClick = () => {
+    if (value === "") return false; // ? Preventing click and Submit if there is no value in Inputbox
+
+    setText(value);
+    setValue("");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <UserInput
+        handleInputChange={handleChange}
+        getUpdateText={text}
+        onButtonClick={onButtonClick}
+        inputValue={value}
+      />
     </div>
   );
 }
